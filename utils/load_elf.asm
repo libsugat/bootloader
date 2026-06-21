e1432856923d4c03acbcd303fe6b003b767293f2
Sugat Bagde <bagdesugat.2006@gmail.com>
2026-06-22 01:42:25 +0530

    Fixed elf loader comments, and fixed problems with elf_loader.

--- utils/load_elf.asm
+++ utils/load_elf.asm
@@ -130,7 +130,7 @@ loop_pht_entries:
     cmp dword [bp + ELF32PH_TYPE], 0x01
     jne elf32phte_nto_load
 elf32phte_to_load:
-    ; Branch A
+    ; Branch A: If ph_type == PT_LOAD
     pushad
 
     ; Extract data to and free up ebp for use
@@ -140,9 +140,11 @@ elf32phte_to_load:
     and ebx, 0x01ff                 ; the the place relative to active sector of loading, ebx % 512
     mov ecx, [bp + ELF32PH_FILESZ]  ; Bytes to load in in memory
     mov edi, [bp + ELF32PH_PADDR]   ; This is the place where it is expected to load elf segment
+    ; NOTE: Do not update edi, or even try to use it unless wrapped with push and pop edi
+    ;       to keep a consistant cursor to destination
 
-    ; Load data from first unaligned sector
-    ;   Load the active sector
+    ; ------------ Load data from first unaligned sector -----------
+    ;   Load the active sector to bounce buffer
     pushad
     xor ebp, ebp
     shr eax, 9 ; divide by 512
@@ -154,79 +156,90 @@ elf32phte_to_load:
     mov dl, [drive_with_elf]
     call read_sectors
     jnc no_error_sector_read
+    ; If error fix the stack alignment and just jump to exit with error
     popad
     popad
     jmp load_elf32__error_exit
 no_error_sector_read:
-    popad
+    popad ; restore the cpu state before loading the fist sector to bounce buffer
 
-    ; Calculate the number of bytes to load, for unaligned sectors
+    ; Calculate the number of Bytes, that are available 
+    ; after the calculated offset form the sector start
+    ; and store in ebp
     mov ebp, eax        ; ecx = file_offset (eax is preserved!)
     and ebp, 511        ; ecx = file_offset % 512 (bytes used in current sector)
-    mov esi, ebp
+    mov esi, ebp        ; esi = loadable_data_offset relative to sector start
     not ebp             ; ecx = -ecx - 1
     lea ebp, [ebx + 512]; ecx = (-ecx - 1) + 513  =>  512 - ecx
 
     push ecx
-    ; TODO: Write code for kernal that might fix in single sector
-    mov ecx, ebp
-    mov edx, ecx   ; Save the Number of bytes we are loading
-    ; Copy into expected location
+    ; ECX = Minimum(ph_filesz(ecx), bytes loadeable from the current sector)
+    cmp ecx, ebp
+    cmovg ecx, ebp ; if ecx > ebp : ecx = ebp
+    mov edx, ecx   ; Save the Number of bytes we are loading, will be used to update values
     add esi, BOUNCE_BUFFER_OFFSET
     ; edi already loaded
     call memcpy_unreal
     pop ecx
-    ; Loading first sector done 
+    ; ------ Loading first sector done  ------
 
-    sub ecx, edx
+
+    ; Update, number of bytes to load, the file_offset and calculate he total sectors to load
     add eax, edx
+    sub ecx, edx
+    jz read_and_load_part_done
     mov edx, ecx
     add edx, 511
     shr edx, 9 ; Divide by 512
 
-    mov dword [current_memory_offset], edi
-
-    ; Load the sectors that span the full sector, the main body sector
-    ; Use edx as counter here
+    ; ----- Load data from consiqutive sectors to the designated memory 
+    ; Use edx as counter of remaining segments
 segment_sector_loading_loop:
-    push edx
+    ; Load the sector to Bounce buffer
     push eax
+    push edx
     push ecx
+    push edi
     xor ebp, ebp
-    shr eax, 9 ; divide by 512
-    add eax, [elf_lba_offset]
-    adc ebp, [elf_lba_offset + 4]
+    shr eax, 9 ; Calculate the the sector number relative to file start
+    add eax, [elf_lba_offset]     ; Convert the sector number relative to file start to LBA 
+    adc ebp, [elf_lba_offset + 4] ; Calculate the upper 4 bytes of LBA
     mov edi, BOUNCE_BUFFER_OFFSET
     mov cx, 0x01
     xor edx, edx
     mov dl, [drive_with_elf]
     call read_sectors
+    pop edi
     pop ecx
-    pop eax
     pop edx
+    pop eax
 
-    mov edi, [current_memory_offset]
+    ; Copy the required data from bounce buffer to its exact location
     push ecx
+    ; calculate the number of bytes to move
+    ; ecx = min(ecx, 0x200)
     mov ebx, 0x200
     cmp ecx, ebx
     cmovg ecx, ebx
-    mov ebx, ecx
+    mov ebx, ecx    ; Save the the number of bytes being moved
     mov esi, 0xd000
-    ; EDI to already be set
+    ; EDI is already set
     call memcpy_unreal
     pop ecx
-    mov dword [current_memory_offset], edi
 
+    ; Update file offset, number of bytes
     sub ecx, ebx
     add eax, ebx
-    dec edx
+    dec edx ; Decrement the number of sectors remaining
     jnz segment_sector_loading_loop
 
-    ; Loading section like .bss
+read_and_load_part_done:
+
+    ; Loading ph_filesz and ph_memsize fomr the cache
     mov bp, [current_program_header_offset]
     mov ecx, [bp + ELF32PH_MEMSZ]
+    ; Calculate the uninitialized data bytes to be zerod/cleared
     sub ecx, [bp + ELF32PH_FILESZ]
-
     call clear_bytes_unreal
 
 elf32phte_to_load__done:
@@ -240,6 +253,7 @@ loop_pht_entries_adv:
     jnz loop_pht_entries
     ; loop loop_pht_entries ; Jump if ecx != 0
 
+    ; Return the memory address of kernel start
     mov eax, [di + ELF32H_ENTRY]
 
     jmp load_elf32__exit
@@ -249,13 +263,12 @@ load_elf32__exit:
     ret
 
 elf32phte_nto_load:
-    ; Branch B
+    ; Branch B: If ph_type != PT_LOAD
     jmp loop_pht_entries_adv
 
 SECTION .data
 drive_with_elf db 0x0
 elf_lba_offset dq 0x0
-current_memory_offset dd 0x0
 current_program_header_offset dw 0x0
 
 SECTION .rodata
